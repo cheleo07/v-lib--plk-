a92211f13e1270102a23684de7edaa52e322487d
Leo CHEVALIER <lchevalier@geiffel.fr>
2019-10-18 13:29:38 +0200

    rm

--- PPEVLIB/controleur/controleurEmprunt.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once 'vue/vueEmprunt.php' ;
--- PPEVLIB/vue/bas.php
+++
@@ -1,7 +0,0 @@
-<p>
-Gestion des abonnements : Léo <br/>
-Gestion des maintenances : Kévin <br/>
-Gestion des emprunts et des retours : Pierre <br/>
-</p>
-
-
