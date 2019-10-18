a85743269c5e9b7936502928c75a7dd21581f7da
cheleo07 <leo2a2b@gmail.com>
2019-10-18 16:00:12 +0200

    boutons

--- vue/vueAbonnement.php
+++ vue/vueAbonnement.php
@@ -4,7 +4,11 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-		
+	<h1>Abonnements</h1>
+	<input class="bouton bouton1" type="button" value="Pass 24H">
+    <input class="bouton bouton2" type="button" value="Pass 7 Jours">
+    <input class="bouton bouton3" type="button" value="Pass Mensuel">
+    <input class="bouton bouton4" type="button" value="Pass Annuel">
 	</main>
 	<footer>
 		<?php include 'bas.php' ;?>
