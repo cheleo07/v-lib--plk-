5d366b6fe42d696874ae9ac934b5dbd0add7ede9
cheleo07 <leo2a2b@gmail.com>
2019-10-21 18:40:10 +0200

    vue abonnement choisis

--- controleur/controleurAbonnement.php
+++ controleur/controleurAbonnement.php
@@ -1,12 +1,23 @@
 <?php
+$formulaireAbonnement = new Formulaire('post', 'index.php', 'fAbonnement', 'fAbonnement');
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerLabelFor('numPortable', "Numero de votre portable :"), 1);
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerInputTexte('numPortable', 'numPortable', ""   , 1, "",0),1);
+$formulaireAbonnement->ajouterComposantTab();
+
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerLabelFor('mail', "Email :"), 1);
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerInputTexte('mail', 'mail', ""   , 1, "",0),1);
+$formulaireAbonnement->ajouterComposantTab();
+
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerLabelFor('codesecret', 'Code secret (4 chiffres):'), 1);
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerInputTexte('codesecret', 'codesecret', "" ,1,"",0),1);
+
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerLabelFor('codesecret2', 'Confirmation du code secret:'), 1);
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement->creerInputTexte('codesecret2', 'codesecret2', "" ,1,"",0),1);
+$formulaireAbonnement->ajouterComposantTab();
+
+$formulaireAbonnement->ajouterComposantLigne($formulaireAbonnement-> creerInputSubmit('submitConnex', 'submitConnex', 'Valider'),2);
+$formulaireAbonnement->ajouterComposantTab();
+
+$formulaireAbonnement->creerFormulaire();
 require_once 'vue/vueAbonnement.php' ;
-/*if (isset($_BUTTON['b1']))
-{
-    // La tu tape ton code si la personne a cliquer et tu met ta page en include ;
-    include("vue/vueCompte.php");
-}
-else // Donc si elle n'a pas cliqué, si elle vien sur la page pour la première fois
-{
-    // Tu met le contenu de ta page
-    include("url.php");
-}*/
+require_once 'fonctions/formulaire.php';
--- vue/vueAbonnement.php
+++ vue/vueAbonnement.php
@@ -6,21 +6,29 @@
 	<main>
 	<div class="texteAccueil">
 	<h1>Abonnements</h1>
+	<?php 
+	if(!isset($_POST['d1'])AND !isset($_POST['d2'])AND !isset($_POST['d3'])AND !isset($_POST['d4'])){
+	?>
 	<form action="" method="post">
 	<button name="d1" class="bouton bouton1" type="submit"><b>Pass 24H </b><br />1.5€</button>
     <button name="d2" class="bouton bouton2" type="submit"><b>Pass 7 Jours</b><br />7€</button>
     <button name="d3" class="bouton bouton3" type="submit"><b>Pass Mensuel</b><br />10€</button>
     <button name="d4" class="bouton bouton4" type="submit"><b>Pass Annuel</b><br />30€</button>
     </form>
-    <?php 
-    if(isset($_POST['d1']))
-        echo 'd1'; 
-    else if(isset($_POST['d2']))
-        echo 'd2';
-    else if(isset($_POST['d3']))
-        echo 'd3';
-    else if(isset($_POST['d4']))
-        echo 'd4';
+    <?php
+	}
+	if(isset($_POST['d1'])){
+	    echo $formulaireAbonnement->afficherFormulaire();
+	}
+	else if(isset($_POST['d2'])){
+	    echo $formulaireAbonnement->afficherFormulaire();
+    }
+    else if(isset($_POST['d3'])){
+        echo $formulaireAbonnement->afficherFormulaire();
+    }
+    else if(isset($_POST['d4'])){
+        echo $formulaireAbonnement->afficherFormulaire();
+    }
     ?>
 	</main>
 	</div>
